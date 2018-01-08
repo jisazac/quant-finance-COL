76307846ef3e291aadad51a846147febb970dbf3
Juan Esteban <juanisazac@gmail.com>
2018-01-07 22:43:30 -0500

    TODO
    Se agrega archivo de texto con listado de pendientes

---
+++ financial_securities/stock_class.py
@@ -0,0 +1,14 @@
+# stock_class.py
+
+
+class stock:
+    """Class for ordinary stock """
+
+    def __init__(self,ticker,price,dvd_yield,hist_data):
+        self.ticker=ticker
+        self.price=price
+        self.dvd_yield=dvd_yield
+        self.hist_data=hist_data
+
+
+    def
